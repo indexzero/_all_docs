dd47d06eefa434768cdb45c811934a7b53091ffa
Charlie Robbins <charlie.robbins@gmail.com>
2025-02-28 05:08:06 -0500

    [refactor] {get,write}Partition

--- test/index.test.js
+++ test/index.test.js
@@ -3,19 +3,36 @@ const { readFile, unlink } = require('node:fs/promises');
 const { join } = require('node:path');
 const { ok } = require('node:assert/strict');
 
-const { getPartition } = require('../src/index');
+const { 
+  getPartition,
+  writePartition 
+} = require('../src/index');
 
 const fixtures = join(__dirname, 'fixtures');
 
 describe('_all_docs/request', () => {
-  it('getPartition(opts) should write a file', async () => {
+  it('getPartition(opts) returns a valid _all_docs partition', async () => {
     const partition = {
       startKey: '8',
       endKey: '9',
       filename: join(fixtures, '8___9.json')
     };
 
-    await getPartition({ partition });
+    const doc = await getPartition({ partition });
+
+    ok(doc.total_rows > 0);
+    ok(doc.offset > 0);
+    ok(doc.rows.length > 0);
+  });
+
+  it('writePartition(opts) should write a file', async () => {
+    const partition = {
+      startKey: '8',
+      endKey: '9',
+      filename: join(fixtures, '8___9.json')
+    };
+
+    await writePartition({ partition });
     const text = await readFile(partition.filename, 'utf8');
     const doc = JSON.parse(text);
 
