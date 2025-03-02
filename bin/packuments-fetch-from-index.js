4ec533d8790b1f7e4e5f8190051bf1bc4c4cb0b0
Charlie Robbins <charlie.robbins@gmail.com>
2025-03-02 01:55:14 -0500

    [bin api] The packuments must flow

---
+++ bin/packuments-fetch-from-index.js
@@ -0,0 +1,40 @@
+const { cachePackumentsSeries, getPackumentsLimit } = require('../src/packument.js');
+const { readFileSync } = require('node:fs');
+const { writeFile } = require('node:fs/promises');
+const { join } = require('node:path');
+const { execSync, exec } = require('node:child_process');
+
+const debug = require('debug')('_all_docs/packuments-fetch-from-index');
+
+const cacheDir = join(__dirname, '..', 'cache');
+const indexFilename = join(cacheDir, '.index');
+const packumentsDir = join(cacheDir, 'packuments');
+
+// const indexEntries = execSync(`head -n 100 ${indexFilename}`, { stdio: [] })
+  // .toString()
+  // .split('\n')
+  // .filter(Boolean)
+  // .map(line => line.trim().split(','));
+
+const indexEntries = readFileSync(indexFilename, 'utf8')
+  .split('\n')
+  .filter(Boolean)
+  .map(line => line.trim().split(','));
+
+(async function () {
+  const limit = 10;
+
+  const packageNames = indexEntries.map(([name]) => name);
+
+  // const packuments = await getPackumentsLimit(packageNames, limit);
+  // console.log('packuments', packuments);
+
+  let cached = 0;
+  cachePackumentsSeries(packageNames, async function writePackument(packument) {
+    const { _id, _rev } = packument;
+
+    debug('cache packument | ', { _id, _rev, cached: ++cached });
+
+    writeFile(join(packumentsDir, `${_id}.json`), JSON.stringify(packument));
+  });
+})();
