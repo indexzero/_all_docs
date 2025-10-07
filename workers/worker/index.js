58c7b5429b0993e313ea1313431494f8e750313d
indexzero <charlie.robbins@gmail.com>
2025-10-06 23:24:23 -0400

    [fix test] Resolve integration tests

--- workers/worker/index.js
+++ workers/worker/index.js
@@ -2,4 +2,3 @@ export { default as app } from './app.js';
 export * from './processors/index.js';
 export { Processor } from './processor.js';
 export * from './interfaces.js';
-export { createStorageDriver } from './storage.js';
